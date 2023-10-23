6260981828ca355104ae2ff92be7769fe8292346
Amol <amol@nxglabs.in>
2023-10-23 17:29:32 +0530

    Update mongo-init.sh

--- mongo-init.sh
+++ mongo-init.sh
@@ -6,7 +6,11 @@ echo "Starting MongoDB initial data import..."
 sleep 10
 
 # Import the data
+mongosh use OpenSignDB
+
 mongoimport --host localhost --db OpenSignDB --collection _Role --type json --file /docker-entrypoint-initdb.d/legadraGCva._Role.json --jsonArray
 mongoimport --host localhost --db OpenSignDB --collection _SCHEMA --type json --file /docker-entrypoint-initdb.d/legadraGCva._SCHEMA.json --jsonArray
+
+
 
 echo "MongoDB initial data import done."
